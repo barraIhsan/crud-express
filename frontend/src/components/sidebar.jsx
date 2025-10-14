daf39f77e5e0f908bdc09887b5a87014d644fb6b
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-10-14 14:27:45 +0700

    style: use shadcn's sidebar

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -1,30 +1,52 @@
-import { useState } from "react";
-import { X, Menu, User, Package } from "lucide-react";
+import { Package } from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "./ui/sidebar";
+import { Users } from "lucide-react";
 import { Link } from "react-router-dom";
 
-export default function Sidebar() {
-  const [isOpen, setIsOpen] = useState(true);
-  
+const items = [
+  {
+    title: "Users",
+    url: "/dashboard/user",
+    icon: Users,
+  },
+  {
+    title: "Products",
+    url: "/dashboard/product",
+    icon: Package,
+  },
+];
+
+export default function sidebar() {
   return (
-    <aside className={`${isOpen ? "w-64" : "w-16"} bg-white shadow-md p-4 flex flex-col transition-all duration-300`}>
-      <div className="flex items-center justify-between mb-8">
-        {isOpen && <h2 className="">Dashboard</h2>}
-        <button onClick={() => setIsOpen(!isOpen)}>
-          {isOpen ? <X /> : <Menu />}
-        </button>
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <Link to="/dashboard/user" className="flex items-center gap-4 p-2 hover:bg-gray-200 rounded">
-          {isOpen && <span>User</span>}
-          <User />
-        </Link>
-
-        <Link to="/dashboard/product" className="flex items-center gap-4 p-2 hover:bg-gray-200 rounded">
-          {isOpen && <span>Product</span>}
-          <Package />
-        </Link>
-      </div>
-    </aside>
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Dashboard</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link to={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
   );
 }
